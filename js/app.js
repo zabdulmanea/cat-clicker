50fcbca54bedde5949c45045b1c8ccd817606167
Zainab-Abdulmanea <zezo.saz@gmail.com>
2019-02-17 01:58:44 +0300

    Increment cat clicker number when click on the cat

--- js/app.js
+++ js/app.js
@@ -5,4 +5,8 @@ function myFunction() {
         document.getElementById('poplinre_counter').textContent = parseInt(clicks_counter) + 1;
     };
 
+    document.getElementById("chewie_img").onclick = function () {
+        var clicks_counter = document.getElementById("chewie_counter").innerText;
+        document.getElementById('chewie_counter').textContent = parseInt(clicks_counter) + 1;
+    };
 }
