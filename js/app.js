66527b1a76d199e0f1130d8857340926db2f78e1
Zainab-Abdulmanea <zezo.saz@gmail.com>
2019-02-17 02:11:36 +0300

    Increment the cat counter when clicking on the cat image

--- js/app.js
+++ js/app.js
@@ -7,4 +7,17 @@ $(document).ready(function () {
         var target = $(this).attr('href');
         $('.cat' + target).toggle();
     });
+
+    // Increment the cat counter when clicking on the cat image 
+    $(".cat img").click(function (event) {
+        // get the counter span element
+        var clicks_span = $(this).next().children()[0];
+        // get the number of counts
+        var clicks_counter = clicks_span.textContent;
+        // Increase the counter by 1
+        clicks_span.textContent = parseInt(clicks_counter) + 1;
+
+        // var img_id = $(this).attr('id');
+        // var clicks_id = clicks_span.getAttribute('id');
+    });
 });
