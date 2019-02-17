5e0981602d54a076d5288b30b1eef6c103f7f520
Zainab-Abdulmanea <zezo.saz@gmail.com>
2019-02-17 18:56:43 +0300

    Provide the model data

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
-$(document).ready(function () {
-    // Show and hide cat details based on clicking cat name
-    // Hide all cats details
-    $('.cat').hide();
-    $(".cats-list a").click(function (event) {
-        $('.cat').hide();
-        var target = $(this).attr('href');
-        $('.cat' + target).toggle();
-    });
+$(function () {
 
-    // Increment the cat counter when clicking on the cat image 
-    $(".cat img").click(function (event) {
-        // get the counter span element
-        var clicks_span = $(this).next().children()[0];
-        // get the number of counts
-        var clicks_counter = clicks_span.textContent;
-        // Increase the counter by 1
-        clicks_span.textContent = parseInt(clicks_counter) + 1;
+    /* ====== MODEL ====== */
+    var model = {
+        currentCat: null,
+        cats: [
+            {
+                clickCount: 0,
+                name: 'Poplinre',
+                imgSrc: 'images/poplinre_cat.jpg',
+                imgAttribution: ''
+            },
+            {
+                clickCount: 0,
+                name: 'Chewie',
+                imgSrc: 'images/chewie_cat.jpg',
+                imgAttribution: ''
+            },
+            {
+                clickCount: 0,
+                name: 'Jetske',
+                imgSrc: 'images/jetske_cat.jpg',
+                imgAttribution: ''
+            },
+            {
+                clickCount: 0,
+                name: 'Sumia',
+                imgSrc: 'images/sumia_cat.jpeg',
+                imgAttribution: ''
+            }
+        ]
+    };
 
-        // var img_id = $(this).attr('id');
-        // var clicks_id = clicks_span.getAttribute('id');
-    });
-});
+    /* ====== OCTOPUS ====== */
+
+    /* ====== VIEW ====== */
+}());
